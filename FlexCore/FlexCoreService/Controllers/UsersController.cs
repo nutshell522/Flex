a48f041e96b3e6d73d9b308a741ae136fb0076bb
WUPINYING <sheluckys4@gmail.com>
2023-08-03 09:48:05 +0800

    add UsersController

---
+++ FlexCore/FlexCoreService/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlexCoreService.Controllers
+{
+    [EnableCors("AllowAny")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
