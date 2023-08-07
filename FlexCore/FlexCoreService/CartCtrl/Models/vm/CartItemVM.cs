dc5ebe4eebdc7b74709d50cddda039ac71f05557
Ted <jjboy830227@gmail.com>
2023-08-07 16:26:53 +0800

    增加購物車欄位

--- FlexCore/FlexCoreService/CartCtrl/Models/vm/CartItemVM.cs
+++ FlexCore/FlexCoreService/CartCtrl/Models/vm/CartItemVM.cs
@@ -2,7 +2,7 @@
 {
 	public class CartItemVM
 	{
-		public int Id { get; set; }
+		public int CartItemId { get; set; }
 		public int CartId { get; set; }
 		public int ProductId { get; set; }
 		public int Qty { get; set; }
