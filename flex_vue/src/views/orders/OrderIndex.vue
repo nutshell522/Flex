7235fd903ed6eeff660346f9dffa52e83bbf6d3f
Patrick <u7uyuuuy@gmail.com>
2023-08-07 13:26:11 +0800

    0807-1

--- flex_vue/src/views/orders/OrderIndex.vue
+++ flex_vue/src/views/orders/OrderIndex.vue
@@ -1,20 +1,22 @@
+
 <template>
+    <OrdernavBar></OrdernavBar>
     <div class="container">
-        <table class="table table-striped table-hover">
+        <table class="table table-striped table-hover" style="width:80% ;">
             <thead>
                 <tr>
-                    <td>訂單日期</td>
-                    <td>訂單編號</td>
-                    <td>總數量</td>
-                    <td>總金額</td>
-                    <td>付款方式</td>
-                    <td>發票編號</td>
-                    <td>訂單狀態</td>
+                    <th>訂單日期</th>
+                    <th>訂單編號</th>
+                    <th>總數量</th>
+                    <th>總金額</th>
+                    <th>付款方式</th>
+                    <th>發票編號</th>
+                    <th>訂單狀態</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="item in GetOrders" :key="item.id">
-                    <td>{{ item.ordertime }}</td>
+                    <td>{{ formatOrderTime(item.ordertime) }}</td>
                     <td>{{ item.id }}</td>
                     <td>{{ item.total_quantity }}</td>
                     <td>{{ item.total_price }}</td>
@@ -28,6 +30,7 @@
 </template>
 <script setup>
 
+import OrdernavBar from "@/components/Order/OrdernavBar.vue";
 import { ref, onMounted } from 'vue'
 import axios from "axios";
 
@@ -44,13 +47,27 @@ const loadGetOrders = async () => {
             alert(error);
         });
 }
+const formatOrderTime = (ordertime) => {
+    const dateTimeObject = new Date(ordertime);
+    const year = dateTimeObject.getFullYear();
+    const month = (dateTimeObject.getMonth() + 1).toString().padStart(2, '0');
+    const dates = dateTimeObject.getDate().toString().padStart(2, '0');
+    const hours = dateTimeObject.getHours().toString().padStart(2, '0');
+    const minutes = dateTimeObject.getMinutes().toString().padStart(2, '0');
+    return year + '-' + month + '-' + dates + ' ' + hours + ':' + minutes + '';
+};
 onMounted(() => {
     loadGetOrders()
 })
 </script>
 <style scoped>
-.table>thead>tr>td {
+.table>thead>tr>th {
     background-color: maroon;
     color: white;
+    text-align: center;
+}
+
+.table>tbody>tr>td {
+    text-align: center;
 }
 </style>
