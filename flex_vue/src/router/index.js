c94c05a88a2fb6950a8760c881b3ebe3e58ca895
LanceLun <onlyu1213@gmail.com>
2023-08-07 18:54:10 +0800

    刪除合併衝突

--- flex_vue/src/router/index.js
+++ flex_vue/src/router/index.js
@@ -1,80 +1,75 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/home/Home.vue';
-import User from '../views/user/User.vue';
-import Login from '../views/user/Login.vue';
-import ActivityInfo from '../views/activity/ActivityInfo.vue';
-import ActivitySignUp from '../views/activity/ActivitySignUp.vue';
-const webTitle = 'FLEX - ';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/home/Home.vue";
+import User from "../views/user/User.vue";
+import Login from "../views/user/Login.vue";
+import ActivityInfo from "../views/activity/ActivityInfo.vue";
+import ActivitySignUp from "../views/activity/ActivitySignUp.vue";
+const webTitle = "FLEX - ";
 
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/home/Home.vue';
-import User from '../views/user/User.vue';
-
-import ActivityInfo from '../views/activity/ActivityInfo.vue';
 // 路由設定
 const routes = [
   {
     //http://loaclhost/
-    path: '/',
+    path: "/",
     component: Home,
     meta: { title: `${webTitle}首頁` },
   },
   {
     //http://loaclhost/
-    path: '/cart',
-    component: () => import('../views/home/Cart.vue'),
+    path: "/cart",
+    component: () => import("../views/home/Cart.vue"),
     meta: { title: `${webTitle}購物車` },
   },
   {
     //http://loaclhost/User
-    path: '/user',
+    path: "/user",
     component: User,
     meta: { title: `${webTitle}會員` },
   },
   {
     //http://loaclhost/orders
-    path: '/orders',
-    component: () => import('../views/orders/orderindex.vue'),
+    path: "/orders",
+    component: () => import("../views/orders/orderindex.vue"),
     meta: { title: `${webTitle}訂單` },
   },
   {
     //http://loaclhost/activityInfo
-    path: '/activityInfo',
+    path: "/activityInfo",
     component: ActivityInfo,
     meta: { title: `${webTitle}活動` },
   },
   {
     //http://loaclhost/activitySignUp
-    path: '/activitySignUp',
+    path: "/activitySignUp",
     component: ActivitySignUp,
     meta: { title: `${webTitle}活動報名` },
   },
   {
     //http://loaclhost/Men
-    path: '/men',
-    component: () => import('../views/product/ProductMenLayout.vue'),
+    path: "/men",
+    component: () => import("../views/product/ProductMenLayout.vue"),
     children: [
       {
-        path: '',
-        component: () => import('../views/product/ProductMen.vue'),
+        path: "",
+        component: () => import("../views/product/ProductMen.vue"),
       },
       {
-        path: ':categoryName',
-        component: () => import('../views/product/ProductMen.vue'),
+        path: ":categoryName",
+        component: () => import("../views/product/ProductMen.vue"),
       },
       {
-        path: ':categoryName/:subCategoryName',
-        component: () => import('../views/product/ProductMen.vue'),
+        path: ":categoryName/:subCategoryName",
+        component: () => import("../views/product/ProductMen.vue"),
       },
       {
         // 當 /ProductMenLayout/:id/posts 匹配成功
         // Detial.vue 將被渲染到 ProductMenLayout 的 <router-view> 内部，替換card.vue
-        path: 'detail/:prdouctId',
-        component: () => import('../views/product/ProductDetail.vue'),
+        path: "detail/:prdouctId",
+        component: () => import("../views/product/ProductDetail.vue"),
       },
       {
         //http://loaclhost/Login
-        path: '/login',
+        path: "/login",
         component: Login,
       },
     ],
