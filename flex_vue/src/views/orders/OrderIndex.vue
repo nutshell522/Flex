36533de4b7a61f71041ca35189a44064fb02ad1e
Patrick <u7uyuuuy@gmail.com>
2023-08-09 16:32:11 +0800

    0809-6

--- flex_vue/src/views/orders/OrderIndex.vue
+++ flex_vue/src/views/orders/OrderIndex.vue
@@ -2,10 +2,11 @@
   <OrdernavBar></OrdernavBar>
   <dev id="searchorderout" class="container">
     <dev id="srarchdate" class="col-3">
-      <input type="text" v-model="begintime" @searchInputbegintime="inputbegintime" class="form-control"
+      <input type="text" v-model="begintime" @searchInputbegintime="inputbegintime" class="form-control datePicker"
         placeholder="輸入開始日期">
       ~
-      <input type="text" v-model="endtime" @searchInputendtime="inputendtime" class="form-control" placeholder="輸入結束日期">
+      <input type="text" v-model="endtime" @searchInputendtime="inputendtime" class="form-control datePicker"
+        placeholder="輸入結束日期">
     </dev>
     <div id="searchorder" class="col-3">
       <input type="text" v-model="keyword" @searchInput="inputhandler" class="form-control" placeholder="輸入名稱">
@@ -21,8 +22,8 @@
 
     <button @click="setTypeValue('3')">課程</button>
   </div>
-  <div class="container">
-    <table class="table table-striped table-hover" style="width: 100%">
+  <div class="container" v-if="Type == 1">
+    <table class="table" style="width: 100%;">
       <thead>
         <tr>
           <th></th>
@@ -48,8 +49,175 @@
             <td>{{ item.order_status }}</td>
           </tr>
           <tr>
-            <td colspan="7">
-              <table class="table table-striped table-hover" style=" text-align: left;"
+            <td colspan="8">
+              <table class="table " style="text-align: left;border:2px solid black;"
+                v-show="expandedItems.includes(item.id)">
+                <tr>
+                  <td colspan="4" style="text-align: right;">
+                    <div style="text-align: right;">
+                      <button class="btn btn-primary" style="margin-right: 30px;">取消</button>
+                      <button class="btn btn-primary">退貨</button>
+                    </div>
+                  </td>
+                </tr>
+                <tr v-for="orderItem in item.orderItems" :key="orderItem.id">
+                  <td>商品名稱：{{ orderItem.product_name }}</td>
+                  <td>數量：{{ orderItem.quantity }}</td>
+                  <td>價格：{{ orderItem.per_price }}</td>
+                  <td>規格：{{ orderItem.items_description }}</td>
+                </tr>
+                <hr>
+                <tr style="justify-content: center;">
+                  <td colspan="8">
+                    <table class="" style="text-align: left;" v-show="expandedItems.includes(item.id)">
+                      <tr>
+                        <td style="padding:0 200px 80px 20px;border-right:2px solid black;width: 500px;">
+                          <h3>收件資訊</h3>
+                          <br>
+                          <div>地址：{{ item.recipient_address }}</div>
+                          <br>
+                          <div>姓名:{{ item.receiver }}</div>
+                          <br>
+                          <div>電話:{{ item.cellphone }}</div>
+                        </td>
+                        <td id="orderItemDetail" style="padding-left: 30px;width: 800px;">
+                          <h6>運費</h6>
+                          <div>{{ item.freight }}</div>
+                          <h6>運費折扣</h6>
+                          <h6>已使用優惠券</h6>
+                          <div>{{ item.coupon_name }}</div>
+                          <h6>優惠券折扣</h6>
+                          <div>{{ item.coupon_discount }}</div>
+                          <h6>訂單金額</h6>
+                          <div>{{ item.total_price }}</div>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+
+        </template>
+      </tbody>
+    </table>
+  </div>
+  <div class="container" v-if="Type == 2">
+    <table class="table" style="width: 100%;">
+      <thead>
+        <h3>我報名的活動</h3>
+      </thead>
+      <tbody>
+        <template v-for="item in GetOrders" :key="item.id">
+          <tr v-for="orderItem in item.orderItems" :key="orderItem.id">
+            <td style=" text-align: left;">
+              <div>活動名稱：{{ orderItem.product_name }}</div>
+              <div>活動時間:{{ item.order_description }}</div>
+              <div>活動地點：{{ item.recipient_address }}</div>
+              <div>活動講師:{{ item.receiver }}</div>
+              <div>費用：{{ orderItem.per_price }}</div>
+            </td>
+            <td><button @click="toggleDetails(item.id)">收合/展開</button></td>
+          </tr>
+          <tr>
+            <td colspan="8">
+              <table class="table " style=" text-align: left;border:2px solid black;"
+                v-show="expandedItems.includes(item.id)">
+                <tr style="justify-content: center;">
+                  <td colspan="8">
+                    <table class="" style="text-align: left;" v-show="expandedItems.includes(item.id)">
+                      <tr>
+                        <td style="padding:10px 200px 20px 20px;width: 500px;">
+                          <h3>訂單詳請</h3>
+                          <div>購買時間：{{ formatOrderTime(item.ordertime) }}</div>
+                          <div>訂單編號:{{ item.id }}</div>
+                          <div>發票編號{{ item.receipt }}</div>
+                          <button class="btn btn-success">取消</button>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+
+        </template>
+      </tbody>
+    </table>
+  </div>
+  <div class="container" v-if="Type == 3">
+    <table class="table" style="width: 100%;">
+      <thead>
+        <h3>我報名的課程</h3>
+      </thead>
+      <tbody>
+        <template v-for="item in GetOrders" :key="item.id">
+          <tr v-for="orderItem in item.orderItems" :key="orderItem.id">
+            <td style=" text-align: left;">
+              <div>課程名稱：{{ orderItem.product_name }}</div>
+              <div>課程時間:{{ item.order_description }}</div>
+              <div>課程地點：{{ item.recipient_address }}</div>
+              <div>課程講師:{{ item.receiver }}</div>
+            </td>
+            <td><button @click="toggleDetails(item.id)">收合/展開</button></td>
+          </tr>
+          <tr>
+            <td colspan="8">
+              <table class="table " style=" text-align: left;border:2px solid black;"
+                v-show="expandedItems.includes(item.id)">
+                <tr style="justify-content: center;">
+                  <td colspan="8">
+                    <table class="" style="text-align: left;" v-show="expandedItems.includes(item.id)">
+                      <tr>
+                        <td style="padding:10px 200px 20px 20px;width: 500px;">
+                          <h3>訂單詳請</h3>
+                          <div>購買時間：{{ formatOrderTime(item.ordertime) }}</div>
+                          <div>訂單編號:{{ item.id }}</div>
+                          <button class="btn btn-success">取消</button>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+
+        </template>
+      </tbody>
+    </table>
+  </div>
+  <div class="container" v-if="Type == 4">
+    <table class="table" style="width: 100%;">
+      <thead>
+        <tr>
+          <th></th>
+          <th>訂單日期</th>
+          <th>訂單編號</th>
+          <th>總數量</th>
+          <th>總金額</th>
+          <th>付款方式</th>
+          <th>發票編號</th>
+          <th>訂單狀態</th>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-for="item in GetOrders" :key="item.id">
+          <tr>
+            <td><button @click="toggleDetails(item.id)">收合/展開</button></td>
+            <td>{{ formatOrderTime(item.ordertime) }}</td>
+            <td>{{ item.id }}</td>
+            <td>{{ item.total_quantity }}</td>
+            <td>{{ item.total_price }}</td>
+            <td>{{ item.pay_method }}</td>
+            <td>{{ item.receipt }}</td>
+            <td>{{ item.order_status }}</td>
+          </tr>
+          <tr>
+            <td colspan="8">
+              <table class="table " style=" text-align: left;border:2px solid black;"
                 v-show="expandedItems.includes(item.id)">
                 <tr v-for="orderItem in item.orderItems" :key="orderItem.id">
                   <td>商品名稱：{{ orderItem.product_name }}</td>
@@ -57,9 +225,38 @@
                   <td>價格：{{ orderItem.per_price }}</td>
                   <td>規格：{{ orderItem.items_description }}</td>
                 </tr>
+                <hr>
+                <tr style="justify-content: center;">
+                  <td colspan="8">
+                    <table class="" style="text-align: left;" v-show="expandedItems.includes(item.id)">
+                      <tr>
+                        <td style="padding:0 200px 80px 20px;border-right:2px solid black;width: 500px;">
+                          <h3>收件資訊</h3>
+                          <div>地址：{{ item.recipient_address }}</div>
+                          <br>
+                          <div>姓名:{{ item.receiver }}</div>
+                          <br>
+                          <div>電話{{ item.cellphone }}</div>
+                        </td>
+                        <td id="orderItemDetail" style="padding-left: 30px;width: 800px;">
+                          <h6>運費</h6>
+                          <div>{{ item.freight }}</div>
+                          <h6>運費折扣</h6>
+                          <h6>已使用優惠券</h6>
+                          <div>{{ item.coupon_name }}</div>
+                          <h6>優惠券折扣</h6>
+                          <div>{{ item.coupon_discount }}</div>
+                          <h6>訂單金額</h6>
+                          <div>{{ item.total_price }}</div>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
               </table>
             </td>
           </tr>
+
         </template>
       </tbody>
     </table>
@@ -69,6 +266,8 @@
 import OrdernavBar from "@/components/Order/OrdernavBar.vue";
 import { ref, onMounted } from "vue";
 import axios from "axios";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 
 const GetOrders = ref([]);
 const Type = ref("");
@@ -131,13 +330,20 @@ const toggleDetails = (itemId) => {
   }
 }
 
-
 onMounted(() => {
   Type.value = 1;
+  flatpickr(".datePicker", {
+    enableTime: false,
+    dateFormat: "Y-m-d",
+  });
   loadGetOrders();
 });
 </script>
 <style scoped>
+#orderItemDetail>h6 {
+  color: red;
+}
+
 .table>thead>tr>th {
   background-color: maroon;
   color: white;
