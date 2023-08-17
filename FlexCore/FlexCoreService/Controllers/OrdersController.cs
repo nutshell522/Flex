43f86415d41a311c8242a1854526e3899bcfa85e
Patrick <u7uyuuuy@gmail.com>
2023-08-17 17:01:50 +0800

    0817-5

--- FlexCore/FlexCoreService/Controllers/OrdersController.cs
+++ FlexCore/FlexCoreService/Controllers/OrdersController.cs
@@ -14,11 +14,11 @@ namespace FlexCoreService.Controllers
 	public class OrdersController : ControllerBase
 	{
 		private readonly AppDbContext _context;
-
 		public OrdersController(AppDbContext context)
 		{
 			_context = context;
 		}
+
 		[HttpGet("GetOrders")]
 		public async Task<ActionResult<IEnumerable<OrdersIndexVM>>> GetOrders(string? keyword, int? typeId, DateTime? begintime, DateTime? endtime, int? ostatusId)
 		{
@@ -81,10 +81,11 @@ namespace FlexCoreService.Controllers
 				recipient_address = p.recipient_address,
 				order_description = p.order_description,
 				close = (bool)p.close,
+				close_time=p.close_time,
 				total_price = p.total_price,
 				fk_typeId = p.fk_typeId,
 				orderItems = (List<OrderItemsVM>)GetOrderItemsIndex(p.Id)
-			});
+			});		
 			return Ok(result);
 		}
 		private static IEnumerable<OrderItemsVM> GetOrderItemsIndex(int orderId)
@@ -125,10 +126,71 @@ namespace FlexCoreService.Controllers
 
 			if (emp.order_status_Id == 1 || emp.order_status_Id == 2)
 			{
+				DateTime currentTime = DateTime.Now;
+				TimeSpan timeDifference = (TimeSpan)(currentTime - emp.close_time);
+
+				if (timeDifference.TotalDays <= 3)
+				{
+					return "已過退費時間，無法取消";
+				}
 				emp.order_status_Id = 7;
 				_context.Entry(emp).State = EntityState.Modified;
 				await _context.SaveChangesAsync();
 			     emp.close = true;
+				_context.Entry(emp).State = EntityState.Modified;
+				await _context.SaveChangesAsync();
+				return "已取消訂單";
+			}
+			else
+			{
+				return "訂單已寄出，無法取消";
+
+			}
+		}
+		[HttpPut("cancelcourse")]
+		public async Task<string> Cancelcourse(int id)
+		{
+			var db = _context;
+			if (_context.orders == null)
+			{
+				return null;
+			}
+			order emp = await _context.orders.FindAsync(id);
+
+
+			if (emp.order_status_Id == 1 || emp.order_status_Id == 2)
+			{
+				emp.order_status_Id = 7;
+				_context.Entry(emp).State = EntityState.Modified;
+				await _context.SaveChangesAsync();
+				emp.close = true;
+				_context.Entry(emp).State = EntityState.Modified;
+				await _context.SaveChangesAsync();
+				return "已取消預約";
+			}
+			else
+			{
+				return "無法取消";
+
+			}
+		}
+		[HttpPut("cancelProduct")]
+		public async Task<string> CancellProductOrders(int id)
+		{
+			var db = _context;
+			if (_context.orders == null)
+			{
+				return null;
+			}
+			order emp = await _context.orders.FindAsync(id);
+
+
+			if (emp.order_status_Id == 1 || emp.order_status_Id == 2)
+			{
+				emp.order_status_Id = 7;
+				_context.Entry(emp).State = EntityState.Modified;
+				await _context.SaveChangesAsync();
+				emp.close = true;
 				_context.Entry(emp).State = EntityState.Modified;
 				await _context.SaveChangesAsync();
 				return "已取消訂單";
@@ -162,6 +224,28 @@ namespace FlexCoreService.Controllers
 				return "訂單尚未領取";
 			}
 		}
+		[HttpPut("Change")]
+		public async Task<string> ChangeOrders(int orderid)
+		{
+			var db = _context;
+			if (_context.orders == null)
+			{
+				return null;
+			}
+			order emp = await _context.orders.FindAsync(orderid);
+
+			if (emp.order_status_Id == 6)
+			{
+				emp.order_status_Id = 10;
+				_context.Entry(emp).State = EntityState.Modified;
+				await _context.SaveChangesAsync();
+				return "已申請換貨";
+			}
+			else
+			{
+				return "訂單尚未領取";
+			}
+		}
 		[HttpPut("cancelreturn")]
 		public async Task<string> CancelReturn(int orderid)
 		{
@@ -171,19 +255,12 @@ namespace FlexCoreService.Controllers
 				return null;
 			}
 			order emp = await _context.orders.FindAsync(orderid);
-			//if (emp.order_status_Id == 9)
-			//{
-			//	await Task.Delay(TimeSpan.FromMinutes(1));
-			//	emp.close = true;
-			//	_context.Entry(emp).State = EntityState.Modified;
-			//	await _context.SaveChangesAsync();
-			//}
+
 			if (emp.order_status_Id == 9)
 			{
 				emp.order_status_Id = 6;
 				_context.Entry(emp).State = EntityState.Modified;
 				await _context.SaveChangesAsync();
-
 				return "已取消退貨";
 			}
 			else
@@ -192,13 +269,52 @@ namespace FlexCoreService.Controllers
 			}
 			
 		}
+		[HttpPut("cancelChange")]
+		public async Task<string> CancelChange(int orderid)
+		{
+			var db = _context;
+			if (_context.orders == null)
+			{
+				return null;
+			}
+			order emp = await _context.orders.FindAsync(orderid);
 
+			if (emp.order_status_Id == 10)
+			{
+				emp.order_status_Id = 6;
+				_context.Entry(emp).State = EntityState.Modified;
+				await _context.SaveChangesAsync();
+				return "已取消換貨";
+			}
+			else
+			{
+				return "訂單尚未領取";
+			}
+
+		}
+		[HttpPut("setclose")]
+		public async Task<string> Setclose(int orderid)
+		{
+			var db = _context;
+			if (_context.orders == null)
+			{
+				return null;
+			}
+			//order emp = await _context.orders.FindAsync(orderid);
+
+			await Task.Delay(TimeSpan.FromSeconds(10));
+			order emp = await _context.orders.FindAsync(orderid);
+			emp.close = true;
+			_context.Entry(emp).State = EntityState.Modified;
+				await _context.SaveChangesAsync();
+				return "已過鑑賞期";
+			
+		}
 		[HttpPost("NewReturn")]
 		public async Task<string> Return(ReturnVM reDTO, int orderid)
 		{
 			var re = new Return
 			{
-				ID = reDTO.ID,
 				退貨日期 = DateTime.Now,
 				fk訂單 = orderid,
 				退貨轉帳帳號 = reDTO.退貨轉帳帳號,
@@ -237,7 +353,27 @@ namespace FlexCoreService.Controllers
 				});
 			return Reasons;
 		}
+	[HttpPut("activitycolse")]
+	public async Task<string> Activitycolse()
+	{
+		var allOrders = await _context.orders.ToListAsync();
+
+		// 獲取目前時間
+		DateTime currentTime = DateTime.Now;
+
+		foreach (var order in allOrders)
+		{
+			// 判斷是否有 close_time 值且該值小於目前時間
+			if (order.close_time.HasValue && order.close_time.Value < currentTime)
+			{
+				// 將 order_status_Id 改成 6
+				order.order_status_Id = 6;
+			}
+		}
+		// 儲存變更回資料庫
+		await _context.SaveChangesAsync();
+		return "活動已結束";
+		}
 	}
-	
 
 }
