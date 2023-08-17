e44b623ccea40f163d48737a61c27e4ba4c3fe2e
WUPINYING <sheluckys4@gmail.com>
2023-08-17 10:36:39 +0800

    可以把喜愛的商品名稱加到收藏清單

--- flex_vue/src/views/user/Favorites.vue
+++ flex_vue/src/views/user/Favorites.vue
@@ -1,12 +1,34 @@
 <template>
   <navBar></navBar>
   <userBar></userBar>
-  <!-- todo等技術長大大的商品收藏鈕 -->
+  <div class="container">
+    <div class="col-md-6 likeProductCard">
+      <label for="">{{ likeProductName }}</label>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import navBar from '@/components/home/navBar.vue';
 import userBar from '@/components/user/userBar.vue';
+import { ref, onMounted } from 'vue';
+
+const likeProductName = ref('');
+
+//判斷使用者id從localstorege撈出來
+onMounted(() => {
+  const savedLikeProduct = localStorage.getItem('likeProduct');
+  if (likeProductName) {
+    likeProductName.value = savedLikeProduct;
+  }
+  //console.log('savedLikeProduct', savedLikeProduct);
+});
+//顯示
 </script>
 
-<style scoped></style>
+<style scoped>
+.likeProductCard {
+  border: 1px solid;
+  width: 50%;
+}
+</style>
