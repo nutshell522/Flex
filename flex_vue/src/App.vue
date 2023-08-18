ce9e5a54515a5c87847e7043e738fe105fd066e1
WUPINYING <sheluckys4@gmail.com>
2023-08-18 20:23:00 +0800

    google 可以登入囉

--- flex_vue/src/App.vue
+++ flex_vue/src/App.vue
@@ -4,8 +4,7 @@ import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { useGetApiDataStore } from './stores/useGetApiDataStore.js';
-//const store = useGetApiDataStore();
-//console.log(store);
+import { decodeCredential, googleLogout } from 'vue3-google-login';
 </script>
 
 <template>
---
+++ flex_vue/src/components/user/googleLogin.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="loggedIn">
+    <button @click="logout">Logout</button>
+    <h2>name:{{ user.name }}</h2>
+    <h2>email:{{ user.email }}</h2>
+    <img :src="user.picture" />
+  </div>
+  <div v-else>
+    <GoogleLogin :callback="callback" prompt auto-login></GoogleLogin>
+  </div>
+</template>
+
+<script>
+import { decodeCredential, googleLogout } from 'vue3-google-login';
+
+export default {
+  data() {
+    return {
+      loggedIn: false,
+      user: null,
+
+      callback: (response) => {
+        console.log('login');
+        this.loggedIn = true;
+        console.log(response);
+        this.user = decodeCredential(response.credential);
+        console.log(user);
+      },
+    };
+  },
+  methods: {
+    logout() {
+      googleLogout();
+      this.loggedIn = false;
+    },
+  },
+};
+</script>
+
+<style></style>
